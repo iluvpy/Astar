8b3272202d9b8c42ed6956155bad024b50a39b15
mio Manns <miomanns265@gmail.com>
2024-01-14 00:27:41 +0100

    G cost working

--- src/cli.py
+++ src/cli.py
@@ -9,10 +9,12 @@ class Path:
 
 class Node:
     def __init__(self) -> None:
-        self.g = 0
-        self.h = 0
+        self.g = 0 # g_cost
+        self.h = 0 # h_cost 
         self.explored = False
         self.connections = [] # to other nodes
+        self.is_start = False
+        self.is_goal = False
 
     def get_f(self):
         return self.g + self.h
@@ -22,13 +24,21 @@ class Node:
 def print_grid(grid, width):
     for node_list in grid:
         for node in node_list:
-            print("#" if not node.explored else node.get_f(), end="")
+            sign = "#"
+            if node.explored:
+                sign = f" {node.get_f()} "
+            else:
+                if node.is_start:
+                    sign = "S"
+                if node.is_goal:
+                    sign = "G"
+            print(sign, end="")
         print() # endline
 
 
 """
 Algorithm undestanding:
-Note: squares and nodes are essentially the same thing in this case
+Note: squares and nodes are the same thing in this case
 theres a starting node (start) and a goal node (goal),
 I start out with an array containing only the start node, that 
 array serves as an indicator to which nodes need to have their g, h and f costs to be calculated.
@@ -50,13 +60,18 @@ def main():
     print("CLI A*:")
     width = 10
     height = 10
-    grid= [[Node() for _ in range(width)] for _ in range(height)]
+    grid = [[Node() for _ in range(width)] for _ in range(height)]
     start = [randint(0, width - 1), randint(0, height - 1)]
     goal = [randint(0, width - 1), randint(0, height - 1)]
 
+    grid[start[0]][start[1]].is_start = True
+    grid[goal[0]][goal[1]].is_goal = True
+
     unexplored = [start]
     explored = []
+    it = 0
     while True:
+        print(f"iteration: {it}")
         print_grid(grid, width)
 
         # find the index of lowest f cost nodes 
@@ -72,24 +87,37 @@ def main():
         # get actual nodes from indexes
         j, i = unexplored[lowest_f_cost[0]]
         lowest_h_cost = grid[i][j].h 
+        best_node = unexplored[0]
         if len(lowest_f_cost) > 1:
             for index in lowest_f_cost:
                 j, i = unexplored[lowest_f_cost[index]]
                 h_cost = grid[i][j]
                 if h_cost < lowest_h_cost:
-                    lowest_fh_cost = h_cost
+                    lowest_h_cost = h_cost
+                    best_node = index
 
+        # calculate all costs for the nodes around best_node (3x3)
+        # i, j starting pos
+        bni, bnj = best_node
+        starting_index = (bni - 1, bnj - 1) # the top left corner of the 3x3
+        start_i, start_j = starting_index   
+        for i in range(3):
+            for j in range(3):
+                if (i, j) != best_node:
+                    new_g_cost = 10 # horizontal or vertical g_cost
+                    if (abs(i - 1) + abs(j - 1)) > 1: # diagonal g_cost
+                        new_g_cost = 14
+                    node_i = start_i + i
+                    node_j = start_j + j
+                    grid[node_i][node_j].g = new_g_cost
+                    grid[node_i][node_j].explored = True
+                    print(f"new g cost of node x {node_j}, y {node_i}: {new_g_cost}")
+                    unexplored.append([node_i, node_j])
 
-        # # calculate g and h costs of nodes around every unexplored node
-        # for node_index in lowest_f_cost:
-        #     explored.append(node_index)
-        #     for i in range(3):
-        #         for j in range(3):
-        #             if (j, i) != node_index:
-        #                 x, y = node_index
-                        
+        unexplored.remove(best_node) 
 
         input_ = input("enter for next iteration...") 
+        it += 1
         if input_ == "q":
             break
 
